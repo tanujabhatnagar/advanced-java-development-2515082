83c4bb5839f79f1714c2c9351a1c8b666005ea93
Tanuja Bhatnagar <bhatnagartanuja.tb@gmail.com>
2025-06-27 16:41:18 +0530

    substistute principle

--- src/main/java/_02_04/before/ClothingSite.java
+++ src/main/java/_02_04/before/ClothingSite.java
@@ -1,17 +1,32 @@
 package _02_04.before;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClothingSite {
 
     public static void main(String[] args)
     {
         ShirtItem shirtItem=new ShirtItem();
-        checkoutItem(shirtItem);
+      //  checkoutItem(shirtItem);
 
         JacketItem jacketItem=new JacketItem();
-        checkoutItem(jacketItem);
+      //  checkoutItem(jacketItem);
+
+        List<ClothingItem> clothingItemList=new ArrayList<>();
+        clothingItemList.add(shirtItem);
+        clothingItemList.add(jacketItem);
+        checkoutAllItems(clothingItemList);
     }
     static void checkoutItem(ClothingItem item) {
         System.out.println("Item purchased: " + item.getName() + ", price: " + item.getPrice());
+    }
+
+    static void checkoutAllItems(List<ClothingItem> clothingItems){
+        for(ClothingItem clothingItem:clothingItems){
+            checkoutItem(clothingItem);
+        }
+
     }
 
 
