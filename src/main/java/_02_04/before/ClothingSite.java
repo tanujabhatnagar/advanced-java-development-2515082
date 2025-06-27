eb70e3427c7a6719cb6071ad1d51bfb57a0c5d5f
Tanuja Bhatnagar <bhatnagartanuja.tb@gmail.com>
2025-06-27 16:35:39 +0530

    Update ClothingSite.java
    The Liskov substitution principle in Java

--- src/main/java/_02_04/before/ClothingSite.java
+++ src/main/java/_02_04/before/ClothingSite.java
@@ -2,9 +2,19 @@ package _02_04.before;
 
 public class ClothingSite {
 
+    public static void main(String[] args)
+    {
+        ShirtItem shirtItem=new ShirtItem();
+        checkoutItem(shirtItem);
+
+        JacketItem jacketItem=new JacketItem();
+        checkoutItem(jacketItem);
+    }
     static void checkoutItem(ClothingItem item) {
         System.out.println("Item purchased: " + item.getName() + ", price: " + item.getPrice());
     }
+
+
 
 
 }
