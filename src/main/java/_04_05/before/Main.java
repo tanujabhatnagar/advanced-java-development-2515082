a8c371cd7981cc57b3a8f6d13ac3a4a9094ac52e
Tanuja Bhatnagar <bhatnagartanuja.tb@gmail.com>
2025-06-28 13:18:39 +0530

    Streams API
    Streams API and functional programming

--- src/main/java/_04_05/before/Main.java
+++ src/main/java/_04_05/before/Main.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
 
         List<String> fruit = Arrays.asList("apple", "pineapple", "banana", "pear", "strawberry");
-        List<String> capitalisedFruit = processWithoutStreams(fruit);
+        List<String> capitalisedFruit = processWithStreams(fruit);
         System.out.println(capitalisedFruit);
+
     }
 
     static List<String> processWithoutStreams(List<String> fruit) {
@@ -25,6 +28,16 @@ public class Main {
         }
         Collections.sort(capitalisedFruit);
         return capitalisedFruit;
+    }
+
+    static List<String> processWithStreams(List<String> fruit)
+    {
+        return fruit.stream()
+                .map(String::toUpperCase)
+                .filter(item->item.startsWith("P"))
+                .sorted()
+                .collect(Collectors.toList());
+
     }
 
 }
--- src/main/java/_04_06/before/StreamUtils.java
+++ src/main/java/_04_06/before/StreamUtils.java
@@ -9,19 +9,28 @@ public class StreamUtils {
     // This method should a list containing the same elements as the original,
     // but sorted alphabetically.
     List<String> sorted(List<String> list) {
-        return list;
+
+        return list.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     // This method should return a list only containing items
     // from the original list that begin with T.
     List<String> beginningWithT(List<String> list) {
-        return list;
+
+        return list.stream()
+                .filter(item-> item.startsWith("T"))
+                .collect(Collectors.toList());
     }
 
     // This method should a list containing the same elements as the original,
     // but every String should be all capitalized.
     List<String> upperCase(List<String> list) {
-        return list;
+
+        return list.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
     }
 
 
@@ -29,7 +38,12 @@ public class StreamUtils {
     // filter the elements to only get the words beginning with T, and
     // capitalise every string.
     List<String> everythingCombined(List<String> list) {
-        return list;
+
+        return list.stream()
+                .map(String::toUpperCase)
+                .filter(item-> item.startsWith("T"))
+                .sorted()
+                .collect(Collectors.toList());
     }
 
 
