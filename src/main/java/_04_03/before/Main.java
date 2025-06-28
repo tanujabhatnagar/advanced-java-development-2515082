7c4d6dd579d91794d131e25ceae3b24a8def72a4
Tanuja Bhatnagar <bhatnagartanuja.tb@gmail.com>
2025-06-28 13:02:24 +0530

    Method Reference in java
    Method Reference in java

--- src/main/java/_04_03/before/Main.java
+++ src/main/java/_04_03/before/Main.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-    }
 
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        numbers.forEach(NumberUtils::evenOrOdd);
+    }
 }
