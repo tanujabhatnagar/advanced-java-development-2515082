a7ca2271a35958c6ed32041b9c745ff690b8755b
Tanuja Bhatnagar <bhatnagartanuja.tb@gmail.com>
2025-07-06 12:29:15 +0530

    IO Reader

--- src/main/java/_06_02/before/ScannerExample.java
+++ src/main/java/_06_02/before/ScannerExample.java
@@ -1,8 +1,25 @@
 package _06_02.before;
 
+import java.util.Scanner;
+
 public class ScannerExample {
 
     public static void main(String[] args) {
+        System.out.println("Please enter your name: ");
+        Scanner scanner =new Scanner(System.in);
+        String name= scanner.nextLine();
+        System.out.println(name);
+
+        System.out.println("Please enter your age: ");
+    //    Scanner scanner =new Scanner(System.in);
+        int age= scanner.nextInt();
+        System.out.println(age);
+        scanner.nextLine();
+        System.out.println("Please enter your occupation: ");
+        //    Scanner scanner =new Scanner(System.in);
+        String occupation= scanner.nextLine();
+        System.out.println(occupation);
+
 
     }
 
--- src/main/java/_06_03/before/BufferedReaderExample.java
+++ src/main/java/_06_03/before/BufferedReaderExample.java
@@ -1,9 +1,25 @@
 package _06_03.before;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BufferedReaderExample {
 
     public static void main(String[] args) {
+try {
+    BufferedReader reader = new BufferedReader(new FileReader(("/Users/tanujabhatnagar/Desktop/advanced-java-development-2515082/src/main/java/_06_03/example.txt")));
+    String firstLine=reader.readLine();
+    System.out.println("first : "+firstLine);
+    StringBuilder stringBuilder=new StringBuilder();
+    reader.lines().forEach(line-> stringBuilder.append(line+ " "));
+    System.out.println("Rest of the lines");
+    System.out.println(stringBuilder);
 
-
+}catch (IOException e)
+{
+ e.printStackTrace();
+}
     }
 }
