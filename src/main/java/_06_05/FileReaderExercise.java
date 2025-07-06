3cc740035c2b76df8d055366154f8a2f2a79adae
Tanuja Bhatnagar <bhatnagartanuja.tb@gmail.com>
2025-07-06 13:01:48 +0530

    File Reader

--- src/main/java/_06_05/FileReaderExercise.java
+++ src/main/java/_06_05/FileReaderExercise.java
@@ -1,11 +1,21 @@
 package _06_05;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileReaderExercise {
 
     // This method should return the first line of BufferedReaderVsScanner.txt.
     String getFirstLine() {
         String firstLine = "";
-
+        try(  Scanner scanner=new Scanner(new FileReader("/Users/tanujabhatnagar/Desktop/advanced-java-development-2515082/src/main/java/_06_05/BufferedReaderVsScanner.txt"))) {
+            firstLine=scanner.nextLine();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         return firstLine;
     }
 
@@ -13,7 +23,14 @@ public class FileReaderExercise {
     // as a single String.
     String getWholeFile() {
         String wholeFile = "";
-
+        try (BufferedReader bufferedReader=new BufferedReader(new FileReader("/Users/tanujabhatnagar/Desktop/advanced-java-development-2515082/src/main/java/_06_05/BufferedReaderVsScanner.txt"))){
+            String firstLine = bufferedReader.readLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            bufferedReader.lines().forEach(stringBuilder::append);
+            wholeFile=stringBuilder+"";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return wholeFile;
     }
 
